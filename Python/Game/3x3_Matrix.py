eff9ee928c33e3d07544d811746b4cbf58ac42f0
Tim <46498661+tziitim@users.noreply.github.com>
2019-02-25 17:49:17 -0500

    3x3_Matrix.py
    -Small change to the method of determining the winner, unsuccessful.
    -Still trying to find an algorithm that does it efficiently

--- Python/Game/3x3_Matrix.py
+++ Python/Game/3x3_Matrix.py
@@ -2,10 +2,26 @@ import math
 
 blank = 1;
 
+SIZE = 3;
+
 Matrix = [[0 for x in range(3)] for y in range(3)];
 
+
+def getWinner():#THIS FUNCTION IS INCOMPLETE!!!  We still need to fix over detection
+    total = sum(map(sum, Matrix))
+    total = total % SIZE
+    if total >= 4:
+        return "TIED!" #Its a tie
+    if total % 2 == 1:
+        return "O!";#X wins
+    if total % 2 == 0:
+        return "Sadly, X...";#O Wins
+    return 	10;#Game is not over.
+
+
+
 def istaken(pos):
-    if Matrix[int(pos % 3)][int((pos/3))] == 0:
+    if Matrix[int(pos % SIZE)][int((pos/SIZE))] == 0:
         return 0;
     return 1;
 
@@ -24,17 +40,33 @@ def playertwo():
     #y = input();
     if istaken(xy):
         xy = max(min(int(input()),8),0);
-    Matrix[int(xy % 3)][int((xy/3))] = 2;							
+    Matrix[int(xy % SIZE)][int((xy/SIZE))] = 2;
+
+
+def isOver():
+    for i in range(SIZE):
+        for j in range(SIZE):
+            if Matrix[i][j] == 0:
+                return 0;
+    return 1;
+
+
 def tictactoe():
+    if isOver():
+        winner = getWinner();
+        
+        return winner;
+	
     print("Your computer has succsesfully acquired AIDS");
     printboard();
     playerone();
     printboard();
     playertwo();
+    
     #
     #Matrix[max(min(int(x),3),0)-1][max(min(int(y),3),0)-1] = 1;
     
-    tictactoe();
+    return tictactoe();
     
     
     
@@ -56,7 +88,8 @@ def p(i):
     return " ";
     
 def main(args):
-    tictactoe();
+    winner = tictactoe();
+    print("the winner is " + winner + "\n")
     return 0
 if __name__ == '__main__':
     import sys
