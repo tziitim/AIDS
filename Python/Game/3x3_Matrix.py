759a96054b58b2926cc7312cc45fb8fa3f7385ee
Tim <46498661+tziitim@users.noreply.github.com>
2019-03-11 18:54:16 -0400

    Tic-Tac-Toe 3x3
    -Finished writing the game

--- Python/Game/3x3_Matrix.py
+++ Python/Game/3x3_Matrix.py
@@ -18,22 +18,43 @@ def getWinner():#THIS FUNCTION IS INCOMPLETE!!!  We still need to fix over detec
     #    return "Sadly, X...";#O Wins
     #return     10;#Game is not over.
     
-    for i in range(0,SIZE):
+    for j in range(0,SIZE):
         x = 1;
         o = 1;
-        for j in range(0,SIZE):
-            for k in range(0,SIZE):
-                if Matrix[j][k] != 1:
-                    print("Matrix[" + str(j) + "]["+ str(k) + "] != X");
-                    x = 0
-                if Matrix[j][k] != 2:
-                    print("Matrix[" + str(j) + "]["+ str(k) + "] != O");
-                    o = 0
+        for k in range(0,SIZE):
+            if Matrix[j][k] != 1:
+                print("Matrix[" + str(j) + "]["+ str(k) + "] != X");
+                x = 0
+            if Matrix[j][k] != 2:
+                print("Matrix[" + str(j) + "]["+ str(k) + "] != O");
+                o = 0
         if o == 1:
             return 'O';
         if x == 1:
             return 'X'
             
+    fx = 1;
+    fo = 1;    
+    for j in range(0,SIZE):
+        if Matrix[j][j] != 1:
+            fx = 0;
+        if Matrix[j][j] != 2:
+            fo = 0;
+    if fx == 1:
+        return 'X';
+    if fo == 1:
+        return 'O'; 
+    fx = 1;
+    fo = 1;
+    for j in range(0,SIZE):
+        if Matrix[SIZE-1-j][SIZE-1-j] != 1:
+            fx = 0;
+        if Matrix[SIZE-1-j][SIZE-1-j] != 2:
+            fo = 0;
+    if fx == 1:
+        return 'X';
+    if fo == 1:
+        return 'O'; 
     return -1;
     
 
@@ -46,19 +67,30 @@ def istaken(pos):
 
 def playerone():
     xy = max(min(int(input()),8),0);
-    #x = input();
-    #y = input();
+    
     if istaken(xy):
         xy = max(min(int(input()),8),0);
-    
-    Matrix[int(xy % 3)][int((xy/3))] = 1;
+        print("Please choose a different location on the board, as it is currently occupied.");
+        playerone();
+        return
+    Matrix[int(xy % SIZE)][int((xy/SIZE))] = 1;
+
+IsAIINIT = 0;
+
+def initAI():
+	
+	__import__(AI);
+	
+    IsAIINIT = 1;
 
 def playertwo():
     xy = max(min(int(input()),8),0);
-    #x = input();
-    #y = input();
+    
     if istaken(xy):
         xy = max(min(int(input()),8),0);
+        print("Please choose a different location on the board, as it is currently occupied.");
+        playertwo();
+        return
     Matrix[int(xy % SIZE)][int((xy/SIZE))] = 2;
 
 
@@ -66,6 +98,9 @@ def isOver():
     for i in range(SIZE):
         for j in range(SIZE):
             if Matrix[i][j] == 0:
+                return 0;
+    for i in range(SIZE):
+        if Matrix[i][i] == 0:
                 return 0;
     return 1;
 
@@ -79,9 +114,14 @@ def tictactoe():
     print("Your computer has succsesfully acquired AIDS");
     printboard();
     playerone();
+    winner = getWinner();
+    if winner != -1:
+        return winner;
     printboard();
     playertwo();
-    
+    winner = getWinner();
+    if winner != -1:
+        return winner;
     #
     #Matrix[max(min(int(x),3),0)-1][max(min(int(y),3),0)-1] = 1;
     
@@ -90,16 +130,22 @@ def tictactoe():
     
     
 def printboard():
-    print("   1 2 3\n");
-    print(" 1 "+p(Matrix[0][0]) + "|" + p(Matrix[1][0]) + "|" + p(Matrix[2][0]));
-    print("   "+"-+-+-");
-    print(" 2 "+p(Matrix[0][1]) + "|" + p(Matrix[1][1]) + "|" + p(Matrix[2][1]));
-    print("   "+"-+-+-");
-    print(" 3 "+p(Matrix[0][2]) + "|" + p(Matrix[1][2]) + "|" + p(Matrix[2][2]));
+    #print("   1 2 3\n");
+    #print(" 1 "+p(Matrix[0][0]) + "|" + p(Matrix[1][0]) + "|" + p(Matrix[2][0]));
+    #print("   "+"-+-+-");
+    #print(" 2 "+p(Matrix[0][1]) + "|" + p(Matrix[1][1]) + "|" + p(Matrix[2][1]));
+    #print("   "+"-+-+-");
+    #print(" 3 "+p(Matrix[0][2]) + "|" + p(Matrix[1][2]) + "|" + p(Matrix[2][2]));
+    for j in range(0,SIZE):
+        print("");
+        for k in range(0,SIZE):
+            print(p(Matrix[k][j],j,k),end = "");    
+    print("\n");	
+    
 
-def p(i):
+def p(i,j,k):
     if i == 0:
-        return " ";
+        return  str((j*SIZE)+k);
     if i == 1:
         return "X";
     if i == 2:
@@ -108,6 +154,7 @@ def p(i):
     
 def main(args):
     winner = tictactoe();
+    printboard();
     print("the winner is " + winner + "\n")
     return 0
 if __name__ == '__main__':
