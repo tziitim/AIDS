8f0442974bd3221d018e1a600d2d0ec6abac3970
Tim <46498661+tziitim@users.noreply.github.com>
2019-03-11 18:54:42 -0400

    The AI portion
    -Started coding the AI

---
+++ AI.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+
+import tensorflow as tf;
+
+
+sess = 0# make it null...?
+
+def AIPlayerChoice():
+    #get AI Choice, from neural net
+    
+def TrainAI():
+    #Train Net
+    
+def InitAI():
+    #Init AI
+    sess = tf.Session();
+    
+    sess.run();
+    
+def CloseAI():
+    sess.close();
