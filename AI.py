27d79480fad4c7cbbf078548b2fb4a337837c20a
Tim <46498661+tziitim@users.noreply.github.com>
2019-03-17 20:34:42 -0400

    Bug fixing
    More bugs needs to be fixed

--- AI.py
+++ AI.py
@@ -2,23 +2,49 @@
 
 
 
-import tensorflow as tf
-from tensorflow.keras import layers
+import tensorflow
+
+from settings import *
+
+#from tensorflow.keras import layers
+#import tensorflow.keras as keras
+#from keras.models import Sequential
+#from keras.layers import Dense, Activation
+
+from keras.models import Sequential
+from keras.layers import Dense, Input
+from keras.layers import InputLayer
 
 
+model = 0;
 
 def AIPlayerChoice():
     model();
 	
 
 def InitAI():
-    print("Tensorflow Version: " + tf.VERSION)
-    print("TF.Keras Version" + tf.keras.__version__)
+    #print("Tensorflow Version: " + VERSION)
+    print("TF.Keras Version" + tensorflow.keras.__version__)
     
-    model = tf.keras.Sequential([#Use a seqention model to get an output from 0-(n^M)
-        layers.Dense(64, activation = 'relu', inputshape = )
-        
+    #model = tf.keras.Sequential([#Use a seqention model to get an output from 0-(n^M)
+    #    layers.Dense(64,use_bias=True, activation = 'relu', inputshape = )
+    #]);
+    model = tensorflow.keras.Sequential();
+    model.add(InputLayer(input_shape=(SIZE**D,)))
+    model.add(Dense(9,use_bias=True,activation = 'relu'));#,input_dim=SIZE**D));
+    model.add(Dense(9,use_bias=True,activation = 'relu'));
+    model.add(Dense(9,use_bias=True,activation = 'relu'));
+    model.add(Dense(9,use_bias=True,activation = 'relu'));
+    model.add(Dense(9,use_bias=True,activation = 'relu'));
+    model.add(Dense(9,use_bias=True,activation = 'relu'));
+    
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+    
+    import numpy as np
+    
+def TrainAI():
+    model.fit();
     
 
 def CloseAI():
-		
+    return;
---
+++ settings.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+SIZE = 3;
+D = 2;
