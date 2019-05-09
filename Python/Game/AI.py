42e2d0434259360e8e2b566f46a66b8d55b923fe
Tim <46498661+tziitim@users.noreply.github.com>
2019-05-08 21:21:43 -0400

    AI File
    -Need more samples

--- Python/Game/AI.py
+++ Python/Game/AI.py
@@ -3,7 +3,7 @@
 
 
 import tensorflow
-
+import random
 from settings import *
 
 #from tensorflow.keras import layers
@@ -19,30 +19,32 @@ from keras.layers import InputLayer
 from keras.layers import Dense, Input
 from keras.layers.normalization import BatchNormalization
 
-
+import numpy as numpy
 
 
 
 def AIPlayerChoice(Matrix):
     
+    
+    global sess
     global model
+    global RMAX
     
     if model == None:
-        print("Model is NONE, re-initing...");
-        model = InitAI();
-    
-    #2d Matrix to ... THAT: [[[1,1,2,2,1,0,0,0,2]]]
+        InitAI();
     
     
     
+    init_op = tensorflow.global_variables_initializer();
     
-    MatrixasTensor = tensorflow.convert_to_tensor([[[1,1,2,2,1,0,0,0,2]]]);
     
-    if MatrixasTensor == None:
-        print("Failed to convert Matrix to a tensor!");
-        return None;
+    sess.run(init_op);
     
-    return model(MatrixasTensor);
+    grid = numpy.array([numpy.array(Matrix).ravel()]);
+    
+    prediction = model.predict(grid);
+    
+    return round(prediction[0][0]);
     
 
 
@@ -60,45 +62,17 @@ def InitAI():
     tensorflow.keras.backend.set_session(sess);
     
     
-    ##model = tf.keras.Sequential([#Use a seqention model to get an output from 0-(n^M)
-    ##    layers.Dense(64,use_bias=True, activation = 'relu', inputshape = )
-    ##]);
-    #model = tensorflow.keras.Sequential([Dense(32, input_shape=(784,))]);
-    ##model.add(InputLayer(input_shape=(SIZE**D,)))
-    #model.add(Dense(9,use_bias=True,activation = 'relu'));#,input_dim=SIZE**D));
-    #model.add(Dense(9,use_bias=True,activation = 'relu'));
-    #model.add(Dense(9,use_bias=True,activation = 'relu'));
-    #model.add(Dense(9,use_bias=True,activation = 'relu'));
-    #model.add(Dense(9,use_bias=True,activation = 'relu'));
-    #model.add(Dense(9,use_bias=True,activation = 'relu'));
-    #
-    #model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
-    
-    
-    #x = Input(input_shape)
-    #model = BatchNormalization(axis = 3)(x)
-    
     model = Sequential()
-    model.add(InputLayer(input_shape))
-    
-    #model.add(tensorflow.keras.layers.Flatten)
-    
-    #model.add(BatchNormalization(axis = 3))
-    
-    //model.add(tensorflow.keras.layers.Flatten())
-    
-    model.add(Dense(9,use_bias=True,activation = 'relu'));
-    model.add(Dense(9,use_bias=True,activation = 'relu'));
-    model.add(Dense(9,use_bias=True,activation = 'relu'));
-    model.add(Dense(9,use_bias=True,activation = 'relu'));
-    model.add(Dense(9,use_bias=True,activation = 'relu'));
-    model.add(Dense(9,use_bias=True,activation = 'relu'));
-    
-    #model.add(Dense(1,use_bias=True,activation = 'softmax'));
+    model.add(Dense(81,use_bias=True,activation = 'relu',input_dim=9));
+    model.add(Dense(81,use_bias=True,activation = 'relu'));
+    model.add(Dense(81,use_bias=True,activation = 'relu'));
+    model.add(Dense(81,use_bias=True,activation = 'relu'));
+    model.add(Dense(81,use_bias=True,activation = 'relu'));
+    model.add(Dense(1,use_bias=True,activation = 'relu'));
     
     
     
-    model.compile(optimizer='rmsprop', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer='rmsprop', loss='mean_squared_error', metrics=['accuracy'])
     model.summary();
     
     model = TrainAI(model);
@@ -117,7 +91,33 @@ def TrainAI(model):
     
     #(1,1,2,2,1,0,0,0,2),(1,1,0,1,2,2,0,0,2),(1,2,0,0,1,2,0,0,0),(1,2,2,2,1,1,1,0,2),(1,2,0,0,1,0,0,2,0),(1,2,1,1,2,0,2,0,0),(1,0,1,0,1,2,2,0,2),(2,0,1,0,2,0,1,2,1)}),{8,7,2,8,7,8,7,1,5}
     import numpy as np
-    model.fit([[ [[[1,1,2,2,1,0,0,0,2]]] , [[[1,1,0,1,2,2,0,0,2]]] ]], [[ [[[8]]] , [[[7]]]  ]],epochs = 300,verbose = 1);
+    
+    samples = np.array(
+            [
+            
+                [1,1,2,
+                 2,1,0,
+                 0,0,2],
+                [1,1,0,
+                 1,2,2,
+                 0,0,2],
+                [1,2,0,
+                 0,1,2,
+                 0,0,0],
+                [1,2,2,
+                 2,1,1,
+                 1,0,2],
+                [1,2,0,
+                 0,1,0,
+                 0,2,0]
+                
+            ]);
+            
+            
+    moves = np.array([7,6,8,7,8])
+    
+    
+    model.fit(samples, moves,epochs = 100,verbose = 2);
     return model;
     
 def CloseAI():
@@ -128,54 +128,25 @@ def CloseAI():
 
 if __name__ == '__main__':
     import sys
-    
+    import numpy as numpy
     global sess
+    global model
     
     print("Init AI...");
     
     model = InitAI();
     
-    test = [[[1,1,2,2,1,0,0,0,2]]];
+    test = [
+            [1,1,2,2,1,0,0,0,2]
+           ];
     
     
     print("Initializing all variables...");
-    tensorflow.global_variables_initializer();
-    
-    print("Converting model input to float32 version")
-    floatingtest = tensorflow.cast(test,tensorflow.float32);
-    
-    print("Convert float32 model input to float32 matrix");
-    MatrixasTensor = tensorflow.convert_to_tensor(floatingtest);
-    
-    print("Calling model");
-    tensor = model(MatrixasTensor);
+    init_op = tensorflow.global_variables_initializer();
     
     
-    if sess == None:
-        print("Tensorflow session is null")
-        sess = tensorflow.keras.backend.get_session();
-        if sess == None:
-            print("Keras backend session was also null!");
-            sess = tensorflow.Session();
-            if sess == None:
-                print("SESS STILL NULL!!!! WHY DOST THOU DO THIS UNTO ME!!!")
-                sys.exit(1);
-        
-    #print("Starting Coordinator");
-    #coord = tensorflow.train.Coordinator()
-    #print("Initailizing threads");
-    #threads = tensorflow.train.start_queue_runners(sess=sess,coord=coord)
-    
-    print("Evaluating Tensor");
-    output = tensor.eval(session=sess)
-    
-    #print("Requesting stop via Coordinator");
-    #coord.request_stop();
-    #print("Joining threads...");
-    #coord.join(threads);
-    
-    print("Tensoflow did a thingity!!!!!! :)!!!!! " + str(output) + "\n");
-
-    
+    sess.run(init_op);
+    prediction = model.predict(numpy.array(test));
+    print(prediction[0]);
     
     sys.exit(0);
